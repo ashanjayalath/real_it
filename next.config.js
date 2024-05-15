8a0406215861bec36a96ea7f5006b00f8dc91665
ashanjayalath <shanjayalath225@gmail.com>
2024-05-16 01:49:23 +0530

    remove appostopy and add another code

--- next.config.js
+++ next.config.js
@@ -1,33 +1,19 @@
-// /** @type {import('next').NextConfig} */
-//
-// const nextConfig = {
-//   swcMinify: true,
-//   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-//   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-//   images: {
-//     domains: [
-//       'images.unsplash.com',
-//       'i.ibb.co',
-//       'scontent.fotp8-1.fna.fbcdn.net',
-//     ],
-//     // Make ENV
-//     unoptimized: true,
-//   },
-// };
-//
-// // module.exports = withTM(nextConfig);
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  // Enable SWC minification for better compression
   swcMinify: true,
-  // Define image domains
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
-    domains: ['images.unsplash.com', 'i.ibb.co', 'scontent.fotp8-1.fna.fbcdn.net'],
-    // Unoptimized images for better compatibility
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    // Make ENV
     unoptimized: true,
   },
 };
 
+// module.exports = withTM(nextConfig);
 module.exports = nextConfig;
